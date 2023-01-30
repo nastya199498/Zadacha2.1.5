660fb2e784bf40d176bbf70b152a233a1344b59a
nastya199498 <114832643+nastya199498@users.noreply.github.com>
2023-01-30 16:10:27 +0300

    Zadacha2.1.5

---
+++ src/main/java/koschei/models/Island2.java
@@ -0,0 +1,19 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Island2 {
+    @Autowired
+    private Wood3 wood3;
+
+    public Island2(Wood3 wood3) {
+        this.wood3 = wood3;
+    }
+
+    @Override
+    public String toString() {
+        return ", на острове дерево " + wood3.toString();
+    }
+}
